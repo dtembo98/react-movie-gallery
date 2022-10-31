693c2b49d3482bdd3f12fcb9b2093f2f9c74717f
dtembo98 <dtembo965@gmail.com>
2022-10-31 19:32:33 +0200

    add error boundarry

---
+++ my-movie-app/src/containers/error-page/error-page.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ErrorPage = () => {
+  return (
+    <StyledNotFoundContainer>
+      <StyledErrorTitle>Error</StyledErrorTitle>
+      <StyledErrorDescription>
+        Sorry, Something went wrong please try again later
+      </StyledErrorDescription>
+      <StyledErrorCode>Try to reload</StyledErrorCode>
+    </StyledNotFoundContainer>
+  );
+};
+
+const StyledNotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+const StyledErrorTitle = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+`;
+
+const StyledErrorDescription = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
+const StyledErrorCode = styled.p`
+  font-size: 10px;
+  font-weight: 700;
+`;
--- my-movie-app/src/containers/index.ts
+++ my-movie-app/src/containers/index.ts
@@ -1,1 +1,4 @@
 export * from "./home/home";
+export * from "./no-internet/no-internet";
+export * from "./error-page/error-page";
+export * from "./error-boundary/error-boundary";
